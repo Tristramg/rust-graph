53307fb031295b3d163a8cc95dd3ddbbdf34bad8
Tristram Gräbener <tristramg@gmail.com>
2013-05-01 18:26:40 +0200

    Adapted to git rust, can add nodes
    Yihaa

--- src/graph.rs
+++ src/graph.rs
@@ -1,26 +1,46 @@
 extern mod core;
 
-use core::hashmap::linear::LinearMap;
-use core::hashmap::linear::linear_map_with_capacity;
+use core::hashmap::HashMap;
+use core::hashmap::linear_map_with_capacity;
 
 // #[crate_type = "lib"];
 
 trait Graph<Node, Edge> {
     fn children(&self, Node, &fn(&(Edge, Node)) -> bool);   // iterate on outgoing edges
+    fn add_node(&mut self, Node);
+    fn node_count(&self) -> uint;
+//    fn add_edge(&self, Edge);
+//    fn edge_count(&self) -> int;
+//    fn each_node(&self, &fn(&Node) -> bool);
+//    fn each_edge(&self, &fn(&Edge) -> bool);
 }
 
-impl<E> Graph<int,E> for LinearMap<int, ~[(E,int)]> {
+impl<E> Graph<int,E> for HashMap<int, ~[(E,int)]> {
     fn children(&self, n:int, f : &fn(&(E,int)) -> bool) {
         match self.find(&n) {
             None => (),
             Some(list) => list.each(f)
         };
     }
+
+    fn add_node(&mut self, n:int) {
+        self.insert(n,~[]);
+    }
+
+    fn node_count(&self) -> uint {
+        self.len()
+    }
 }
 
 #[test]
-fn test_1() {
-    let g : ~LinearMap<int, ~[(int,int)]> = ~linear_map_with_capacity(15);
-    let g2 : ~Graph<int,int> = g as ~Graph<int,int>;
-    g2.children(5, |&(e,x)| { io::println(fmt!("got edge from 5: %d -> %d", e, x)); true });
+fn add_node() {
+    let mut g : ~HashMap<int, ~[(int,int)]> = ~linear_map_with_capacity(15);
+    if g.node_count() != 0 {
+        fail!("The graph is not empty");
+    }
+
+    g.add_node(1);
+    if g.node_count() != 1 {
+        fail!("Node not added");
+    }
 }
