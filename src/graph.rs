01bb4ba53928de9efd1548ae3722fcdb2de1ca70
Tristram Gräbener <tristramg@gmail.com>
2013-05-02 22:45:05 +0200

    Generalisation of the graph to any hashable key
    Now we can have strings as nodes

--- src/graph.rs
+++ src/graph.rs
@@ -1,29 +1,32 @@
 extern mod core;
 
+use core::hash::Hash;
 use core::hashmap::HashMap;
 use core::hashmap::linear_map_with_capacity;
 
 // #[crate_type = "lib"];
 
 trait Graph<Node, Edge> {
-    fn children(&self, Node, &fn(&(Node, Edge)) -> bool);   // iterate on outgoing edges
+    fn children(&self, &Node, &fn(&(Node, Edge)) -> bool);   // iterate on outgoing edges
     fn add_node(&mut self, Node);
     fn node_count(&self) -> uint;
     fn add_edge(&mut self, Node, Node, Edge);
-    fn edge_count(&self) -> uint;
 //    fn each_node(&self, &fn(&Node) -> bool);
-    fn each_edge(&self, &fn(Node, Node, Edge) -> bool);
+    fn each_edge(&self, &fn(&Node, &Node, &Edge) -> bool);
+
+    fn edge_count(&self) -> uint;
 }
 
-impl<E> Graph<int,E> for HashMap<int, ~[(int,E)]> {
-    fn children(&self, n:int, f : &fn(&(int,E)) -> bool) {
-        match self.find(&n) {
+
+impl<N:Hash+Eq,E> Graph<N,E> for HashMap<N, ~[(N,E)]> {
+    fn children(&self, n:&N, f : &fn(&(N,E)) -> bool) {
+        match self.find(n) {
             None => (),
             Some(list) => list.each(f)
         };
     }
 
-    fn add_node(&mut self, n:int) {
+    fn add_node(&mut self, n:N) {
         self.insert(n,~[]);
     }
 
@@ -31,17 +34,17 @@ impl<E> Graph<int,E> for HashMap<int, ~[(int,E)]> {
         self.len()
     }
 
-    fn add_edge(&mut self, source:int, target:int, e:E) {
+    fn add_edge(&mut self, source:N, target:N, e:E) {
         match self.find_mut(&source) {
             None => fail!("Source node does not exist"),
             Some(list) => list.push((target, e))
         }
     }
 
-    fn each_edge(&self, f: &fn(int,int,E) -> bool){
+    fn each_edge(&self, f: &fn(&N,&N,&E) -> bool){
         self.each_key(|&source| {
-            self.children(source, |&(target, edge)| {
-                f(source, target, edge);
+            self.children(&source, |&(target, edge)| {
+                f(&source, &target, &edge);
                 true
             });
             true
@@ -71,6 +74,20 @@ fn add_node() {
     }
 }
 
+#[test]
+fn add_node_string() {
+    let mut g : HashMap<~str, ~[(~str,int)]> = linear_map_with_capacity(15);
+    if g.node_count() != 0 {
+        fail!("The graph is not empty");
+    }
+
+    g.add_node(~"Hii");
+    if g.node_count() != 1 {
+        fail!("Node not added");
+    }
+}
+
+#[test]
 fn add_edge() {
     let mut g : HashMap<int, ~[(int,int)]> = linear_map_with_capacity(15);
     if g.edge_count() != 0 {
@@ -80,6 +97,22 @@ fn add_edge() {
     g.add_node(0);
     g.add_node(1);
     g.add_edge(0,1,1);
+
+    if g.edge_count() != 1 {
+        fail!("Not one edge");
+    }
+}
+
+#[test]
+fn add_edge_string() {
+    let mut g : HashMap<~str, ~[(~str,int)]> = linear_map_with_capacity(15);
+    if g.edge_count() != 0 {
+        fail!("The graph is not empty");
+    }
+
+    g.add_node(~"a");
+    g.add_node(~"b");
+    g.add_edge(~"a",~"b",1);
 
     if g.edge_count() != 1 {
         fail!("Not one edge");
