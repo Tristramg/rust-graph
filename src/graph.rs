4cbb75b59213280efe0098c116e1901ae42139e9
Tristram Gräbener <tristramg@gmail.com>
2013-05-09 11:52:28 +0200

    Added a depth first search

--- src/graph.rs
+++ src/graph.rs
@@ -13,6 +13,18 @@ trait Graph<Node, Edge> {
     fn each_node(&self, &fn(&Node) -> bool);
 }
 
+fn dfs_rec<N:Hash+Eq+Copy,E, G:Graph<N,E>>(g: &G, source: &N, visitor: &fn(&N) -> bool, visited: &mut HashMap<N, bool>) {
+    if visitor(source){
+        g.children(source, |&(target,_)| {
+            if !*visited.get(&target) {
+                visited.insert(target, true);
+                dfs_rec(g, &target, visitor, visited);
+            };
+            true
+            });
+    }
+}
+
 fn edge_count<N,E, G:Graph<N,E>>(g: &G) -> int {
     let mut count = 0;
     each_edge(g, |_,_,_| { count += 1; true });
@@ -25,6 +37,15 @@ fn each_edge<N,E, G:Graph<N,E>>(g: &G, f: &fn(&N,&N,&E) -> bool){
         true
     })
 }
+
+fn dfs<N:Hash+Eq+Copy,E, G:Graph<N,E>>(g: &G, source: &N, visitor: &fn(&N) -> bool) {
+    let mut visited = ~HashMap::new();
+    g.each_node(|&n| {visited.insert(n, false); true});
+    visited.insert(*source, true);
+    dfs_rec(g, source, visitor, visited);
+}
+
+
 
 impl<N:Hash+Eq,E> Graph<N,E> for HashMap<N, ~[(N,E)]> {
     fn children(&self, n:&N, f : &fn(&(N,E)) -> bool) {
@@ -45,7 +66,7 @@ impl<N:Hash+Eq,E> Graph<N,E> for HashMap<N, ~[(N,E)]> {
     fn add_edge(&mut self, source:N, target:N, e:E) {
         match self.find_mut(&source) {
             None => fail!("Source node does not exist"),
-            Some(list) => list.push((target, e))
+                 Some(list) => list.push((target, e))
         }
     }
 
@@ -110,5 +131,26 @@ fn add_edge_string() {
 
     if edge_count(&g) != 1 {
         fail!("Not one edge");
+    }
+}
+
+#[test]
+fn dfs_test() {
+    let mut g = HashMap::new();
+    g.add_node(0);
+    g.add_node(1);
+    g.add_node(2);
+    g.add_edge(0,1,1);
+    g.add_edge(1,2,1);
+    g.add_edge(2,0,1);
+
+    let mut nodes = ~[];
+    dfs(&g, &0, |&n| {nodes.push(n); true });
+
+    if nodes.len() != 3 {
+        fail!("Wrong number of elements visited");
+    }
+    if nodes[0] != 0 || nodes[1] != 1 || nodes[2] != 2 {
+        fail!("Wrong elements");
     }
 }
